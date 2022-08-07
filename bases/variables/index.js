fee6539b2eb7aeca4985c0b09ad3d45b5009e635
Mariya <mane44ka86@mail.ru>
2022-08-07 13:25:23 +0500

    variables

---
+++ bases/variables/index.js
@@ -0,0 +1,15 @@
+//1
+let admin, name;
+name = 'John';
+admin = name;
+
+alert(admin);
+
+//2
+const OUR_PLANET_NAME = "Земля";
+const currentUserName = 'John';
+
+//3
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(BIRTHDAY);
